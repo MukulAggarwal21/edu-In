893b0421d481c594c4f0ce745ee319c9a4247963
MUKUL <mukul.2226csit1115@kiet.edu>
2025-03-25 10:11:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import NameSignup from './components/NameSignup'
+function App() {
+
+  return (
+    <>
+      <div>
+        <NameSignup />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NameSignup.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function NameSignup() {
+    return (
+        <div>
+            <div>
+                <p className="absolute top-[42px] left-[597px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#191919] text-[42px] text-center tracking-[0] leading-[normal]">
+                    <span className="[font-family:'Inter-Regular',Helvetica] font-normal text-[#191919] text-[42px] tracking-[0]">
+                        STUDY
+                    </span>
+
+                    <span className="[font-family:'Inter-Bold',Helvetica] font-bold">
+                        table
+                    </span>
+                </p>
+
+                <input className='top-[10px]' type="text" name="" id="" />
+
+            </div>
+
+
+
+        </div>
+    )
+}
